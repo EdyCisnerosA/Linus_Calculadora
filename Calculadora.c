6b4541258b2c543533caff8f34191bba1c35b16c
Edilzar Cisneros Ancheyta <edy.cisneros1893@gmail.com>
2015-10-26 23:29:02 -0600

    Mensajes en ingles

--- Calculadora.c
+++ Calculadora.c
@@ -71,10 +71,10 @@ double Suma (double a, double b)
 
      do{
 
-        printf("Ingrese un numero: ");
+        printf("Enter a number: ");
         scanf("%s", buffer);
         if( (band = validar_es_flotante(buffer)) == false )
-            printf("Porfavor solo enteros o flotantes.\n");
+            printf("Please only integer or floating.\n");
         
      } while( band == false );
 
@@ -82,11 +82,11 @@ double Suma (double a, double b)
 
      do{
         
-        printf("Ingrese un numero: ");
+        printf("Enter a number: ");
         //scanf ("%lf", &b);
         scanf("%s", buffer);
         if( (band = validar_es_flotante(buffer)) == false )
-            printf("Porfavor solo enteros o flotantes.\n");
+            printf("Please only integer or floating.\n");
     
      } while( band ==false );
     
@@ -98,122 +98,122 @@ double Suma (double a, double b)
 
 double Resta (double a, double b)
 {
-    printf("Ingrese un numero: ");
+    printf("Enter a number: ");
     scanf("%lf", &a);
-    printf("Ingrese un numero: ");
+    printf("Enter a number: ");
     scanf("%lf", &b);
     return (a - b);
 }
 
 double Multiplicacion (double a, double b)
 {
-    printf("Ingrese un numero: ");
+    printf("Enter a number: ");
     scanf("%lf", &a);
-    printf("Ingrese un numero: ");
+    printf("Enter a number: ");
     scanf("%lf", &b);
     return (a * b);
 }
 double Division (double a, double b)
 {
-    printf("Ingrese un numero: ");
+    printf("Enter a number: ");
     scanf("%lf", &a);
-    printf("Ingrese un numero: ");
+    printf("Enter a number: ");
     scanf("%lf", &b);
     return (a / b);
 }
 
 double Raiz_cuadrada (double a)
 {
-    printf("\n introduzca la cifra a la que efectuar la raiz: ");
+    printf("\n enter the number to which effect the root: ");
     scanf ("%lf", &a);
     return (sqrt (a));
 }
 double Potencia(double a, double b){
-     printf("\ningrese el numero base para la potencia:");
+     printf("\nenter the base number to pow:");
      scanf ("%lf", &a);
-     printf("Ingrese potencia: ");
+     printf("Enter pow: ");
      scanf("%lf", &b); 
      return(pow(a,b));
 }           
         
 double Logaritmo(double a){
-     printf("Ingrese el valor: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(log(a));
 }
 double Seno(double a){
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(sin(a));
 }
 
 double Coseno(double a){
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(cos(a));
 }
 
 double Tangente(double a){
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(tan(a));
 }
 
 double Arcseno(double a){
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(asin(a));
 }
 
 double Arccoseno(double a){
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(acos(a));
 }
 
 double Arctangente(double a){     
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(atan(a));
 }
 
 double Ceil(double a){
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &a);
      return(ceil(a));
 }
 
 double ArcoTangente2(double x,double y)
 {
-     printf("Datos en el intervalo [-PI,+PI]radianes");
-     printf("\nIngrese un Numero x: ");
+     printf("Data in the range [ -PI , + PI ] radians");
+     printf("\nEnter a number x: ");
      scanf ("%lf", &x);
-     printf("Ingrese un Numero y: ");
+     printf("Enter a number y: ");
      scanf ("%lf", &x);
      return(atan2(y,x));
      
 }
 double CosenoHiperbolico(double x)
 {
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &x);
      return(cosh(x));
 }
 double SenoHiperbolico(double x)
 {
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &x);
      return(sinh(x));
 }
 double TangenteHiperbolico(double x)
 {
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &x);
      return(tanh(x));
 }
 double Exponencial(double x)
 {
-     printf("Ingrese un Numero: ");
+     printf("Enter a number: ");
      scanf ("%lf", &x);
      return(exp(x));
 }
@@ -222,7 +222,7 @@ double Frexp(double x, double frac){
      /* Ingresado un número, lo descompone en un numero (fraccion) que multiplicado por dos y 
      elevado a una potencia da el número inicial (x=mantisa *2^exp) */
      int e;
-     printf("Ingrese un número:");
+     printf("Enter a number:");
      scanf ("%lf", &x);
      frac = frexp(x, &e);
      printf("x = %.2lf = %.2lf * 2^%d\n", x, frac, e);
@@ -232,9 +232,9 @@ double Frexp(double x, double frac){
 double Idexp(double x, double ret){
      /* Eleva al número dos a una potencia dada y lo multiplica por el número ingresado (x*2^exp) */
      int n;
-     printf("Ingrese un número: ");
+     printf("Enter a number: ");
      scanf("%lf", &x); 
-     printf("Ingrese la potencia:");
+     printf("Enter pow:");
      scanf ("%d", &n);
      ret = ldexp(x ,n);
      printf("%f * 2^%d = %f\n", x, n, ret);
@@ -242,40 +242,40 @@ double Idexp(double x, double ret){
 }
 
 double LogB10(double a){
-     printf("Ingrese el numero para el logaritmo:");
+     printf("Enter a number:");
      scanf ("%lf", &a);
      return(log10(a));
 }
 
 double Modf(double x, double pfrac, double pent){
      /* Separa en dos un número flotante, la parte entera y la parte decimal */
-     printf("Ingrese un número flotante (parte entera y parte decimal):");
+     printf("Enter a floating number ( integer and fractional part):");
      scanf ("%lf", &x);
      pfrac = modf(x,&pent);
-     printf("Parte entera: %lf\n", pent);
-     printf("Parte decimal: %lf \n", pfrac);
+     printf("Integer part : %lf\n", pent);
+     printf("Decimal part: %lf \n", pfrac);
      return(0);
 }
 
 double ValorAbsoluto(double a){
-     printf("Ingrese un número (positivo o negativo): ");
+     printf("Enter a number (positive or negative): ");
      scanf ("%lf", &a);
      return(fabs(a));
 }
 
 double Fmod(double a, double b, double resultado){
      /* Devuelve el residuo de la división para un número flotante */
-     printf("Ingrese el divisor: ");
+     printf("Enter the divisor: ");
      scanf ("%lf", &a);
-     printf("Ingrese el dividendo: ");
+     printf("Enter the dividend: ");
      scanf("%lf", &b); 
      resultado = fmod(a,b);
-     printf("Residuo de %f / %f es %lf\n", a, b, resultado);
+     printf("Residue of %f / %f is %lf\n", a, b, resultado);
      return(resultado);
 }
 
 double Floor(double a){
-     printf("Ingrese un número decimal: ");
+     printf("Enter a decimal number: ");
      scanf ("%lf", &a);
      return(floor(a));
 }
@@ -287,34 +287,34 @@ int main(){
     char buffer[9];
 
     while(1){
-	printf("\n\t\tCALCULADORA\n\n");
-	printf("\t 1.- Suma\n");
-	printf("\t 2.- Resta\n");
-	printf("\t 3.- Multplicaci¢n\n");
+	printf("\n\t\tCALCULATOR\n\n");
+	printf("\t 1.- Sum\n");
+	printf("\t 2.- Subtraction\n");
+	printf("\t 3.- Multiplication\n");
 	printf("\t 4.- Division\n");
-	printf("\t 5.- Raiz cuadrada\n");
-	printf("\t 6.- Potencia\n");
-	printf("\t 7.- Logaritmo Natural\n");
-	printf("\t 8.- Seno\n");
-	printf("\t 9.- Coseno\n");
-	printf("\t 10.-Tangente\n");
-	printf("\t 11.-Arco Seno\n");
-	printf("\t 12.-Arco Coseno\n");
-	printf("\t 13.-Arco Tangente\n");
+	printf("\t 5.- Square Root\n");
+	printf("\t 6.- Pow\n");
+	printf("\t 7.- Natural Logarithm\n");
+	printf("\t 8.- Sine\n");
+	printf("\t 9.- Cosine\n");
+	printf("\t 10.-Tangent\n");
+	printf("\t 11.-arc sine\n");
+	printf("\t 12.-Arc Cosine\n");
+	printf("\t 13.-Arc Tangent\n");
 	printf("\t 14.-Ceil\n");
-	printf("\t 15.-Arco Tangente (dos parametros\n");
-    printf("\t 16.-Coseno Hiperbolico\n");
-    printf("\t 17.-Seno Hiperbolico\n");
-    printf("\t 18.-Tangente Hiperbolico\n");
-    printf("\t 19.-Exponencial\n");
+	printf("\t 15.-Arc Tangent (two parameters\n");
+    printf("\t 16.-Hyperbolic Cosine\n");
+    printf("\t 17.-Hyperbolic Sine\n");
+    printf("\t 18.-Hyperbolic Tangent\n");
+    printf("\t 19.-Exponential\n");
     printf("\t 20.-Frexp\n");
     printf("\t 21.-Idexp\n");
-    printf("\t 22.-Logaritmo Base 10\n");
+    printf("\t 22.-Logarithm base ten\n");
     printf("\t 23.-Modf\n");
-    printf("\t 24.-Valor Absoluto\n");
+    printf("\t 24.-Absolute value\n");
     printf("\t 25.-Floor\n");
     printf("\t 26.-Fmod\n");
-	printf("\t\t Elija su Opci¢n:");
+	printf("\t\t Choose your option:");
     
 
     scanf("%s", buffer);
@@ -429,11 +429,11 @@ int main(){
  	   
        default:
           Opcion = 999;
-         printf("Opcion no valida, INTENTE otra vez solo con digitos.");
+         printf("Invalid option , try again only digits.");
 	   }
 
        if(Opcion<999)
-            printf("\n El resultado de la opci¢n elegida es: %lf \n", resultado);
+            printf("\n The result of the chosen option: %lf \n", resultado);
   
   }
 
